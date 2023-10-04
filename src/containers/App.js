09ec2954a74d28cf63255dbbfed629d09eb31137
BritoJV <eng.jvbrito@gmail.com>
2023-10-03 22:09:06 -0300

    updated useEffect to onyly run at the time the mount occurs, similar to componentDidMount() from classes

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,50 +5,6 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-// class App extends Component {
-    // constructor(){
-    //     super()
-    //     this.state = {
-    //         robots: [],
-    //         searchfield: ""
-    //     }
-    // }
-    // componentDidMount(){
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        //     .then(response=> response.json())
-        //     .then(users=>this.setState({robots: users}));
-    // }
-    // onSearchChange = (event) => {
-    //     console.log(event.target.value);
-    //     this.setState({searchfield: event.target.value});
-    // }
-    // render(){
-    //     const {robots, searchfield} = this.state;
-    //     const filteredRobots = robots.filter(robots=>{
-    //         return robots.name.toLowerCase().includes(searchfield.toLowerCase())
-    //         }
-    //     )
-    //     if (!robots.length){
-    //         return (
-    //             <h1 className='tc'>Loading...</h1>
-    //         )
-    //     }
-    //     else{
-    //         return(
-    //         <div className='tc'>
-    //             <h1>robofriends</h1>
-    //             <Searchbox searchChange={this.onSearchChange}/>
-    //             <Scroll>
-    //                 <ErrorBoundry>
-    //                     <Cardlist robots = {filteredRobots} />
-    //                 </ErrorBoundry>
-    //             </Scroll>
-    //         </div>
-    //         );
-    //     }
-    // }
-// }
-
 function App() {
 
     const [robots, setRobots] = useState([]);
@@ -62,8 +18,8 @@ function App() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=> response.json())
             .then(users=>setRobots(users));
-    })
-
+    },[])
+    
     const filteredRobots = robots.filter(robots=>{
         return robots.name.toLowerCase().includes(searchfield.toLowerCase())
         }
