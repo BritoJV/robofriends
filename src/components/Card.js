00a3c50f4002d9995824b581d005ce8b53be31e0
BritoJV <eng.jvbrito@gmail.com>
2023-11-10 18:09:01 -0300

    corrected robot size display, before it was not set as 200x200

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({id,name,email}) =>{
     return (
         <div className='bg-light-green grow ba bw1 br3 pa2 ma2 dib shadow-5 tc'>
-            <img src={`https://robohash.org/${id+name}?200x200`} alt='robots'/>
+            <img src={`https://robohash.org/${id+name}?size=200x200`} alt='robots'/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
